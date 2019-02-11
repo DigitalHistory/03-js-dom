bb1a3b568f32c601190d16c4ecc2efe2b9f9c8bb
Matt Price <matt.price@utoronto.ca>
2019-02-10 22:48:27 -0500

    getting close to 2019 release.
    - all questions now ready.

--- Part3/03-dom-data.js
+++ Part3/03-dom-data.js
@@ -3,14 +3,14 @@
 // this function should return a new string with a valid Wikipedia link of the form
 // "https://en.wikipedia.org/wiki/Some Name"
 function wikiUrl (name) {
-  return 'https://en.wikipedia.org/wiki/' + name;
+  return '';
 }
 
 // given a name, this function should return, as a string, a valid link to a wiki page
 // of the form <a href='link url'>content</a>
 // the function `wikiURL` should make it easier to do this.
 function wikiLink (name) {
-  return '<a href="' + wikiUrl(name) + '">' + name + '</a>';
+  return '';
   // return $()
 }
 
@@ -18,38 +18,19 @@ function wikiLink (name) {
 // set the element's HTML content to a wikiLink whose internal text content is the original
 // element's text content. 
 function wikifyElementHtml (element) {
-  let content =$(element).text(),
-      link=wikiLink(content);
-  // console.log(link);
-  $(element).html(link);
-  return $(element).html();
+  return '';
 }
 
 // passed a class name or other selector, this function should iterate through all
 // the matching elements and wikify their text.  
 function wikifyClass (selector) {
-  let allElements = $(selector);
-  for (e of allElements) {
-    wikifyElementHtml (e);
-  }
+
 }
 
 
 function wikifySelector(selector) {
-  $(selector).
-    map(function () {
-      $(this).html(function () {
-        return '<a href="https://en.Wikipedia.org/wiki/' + $(this).text() + '">' +
-          $(this).text() + '</a>';
-      });
-    }
-    );
-        };
+};
   
-// wikifyClass('.name');
-
-
-
 
 // DO NOT MODIFY -- FOR AUTOMATED TESTING ONLY
 // MODIFYING THIS CODE WILL ALMOST CERTAINLY CAUSE YOUR TESTS TO BREAK
