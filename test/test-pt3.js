70c386311b8dcb5f94609098eabd31cc6a957d41
Matt Price <matt.price@utoronto.ca>
2019-02-10 19:51:22 -0500

    fix typo

--- test/test-pt3.js
+++ test/test-pt3.js
@@ -22,7 +22,7 @@ describe('Reflection Checks (not required unless you are attempting an "A" grade
   });
   it('The total word count for your reflection should be at least 625', function() {
     //let content=fs.readFileSync(`Reflection/${githubid}.md`, 'utf-8');
-    let content=fs.readFileSync(`Reflection/reflection.md``, 'utf-8');
+    let content=fs.readFileSync(`Reflection/reflection.md`, 'utf-8');
     expect(hwc(content), "").to.be.at.least(625);
   });
 });
